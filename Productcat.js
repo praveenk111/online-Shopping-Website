178c95e7f7adcc4300d29e3e14c4b97a863d6271
praveenk111 <58185797+praveenk111@users.noreply.github.com>
2021-05-04 19:02:21 -0400

    Initial commit

---
+++ Productcat.js
@@ -0,0 +1,98 @@
+let products = [
+    {
+        'id': 0,
+        'name': 'White Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'price': 59.99,
+        'pic': 'red_jersey.jpg',
+        'qty_in_stock':200
+    },
+    {
+        'id': 1,
+        'name': 'Yellow Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'price': 49.99,
+        'pic': 'white_jersey.jpg',
+        'qty_in_stock': 133
+    },
+    {
+        'id': 2,
+        'name': 'Blue Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'price': 54.99,
+        'pic': 'black_jersey.jpg',
+        'qty_in_stock': 544
+    },
+    {
+        'id': 3,
+        'name': 'Red Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'price': 129.99,
+        'pic': 'blue_jacket.jpg',
+        'qty_in_stock': 14
+    },
+    {
+        'id': 4,
+        'name': 'Black Jersey',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'price': 124.99,
+        'pic': 'cap.jpg',
+        'qty_in_stock': 655
+    },
+    {
+        'id': 5,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'price': 24.99,
+        'pic': 'champion_league_flag.jpg',
+        'qty_in_stock': 321
+    },
+    {
+        'id': 6,
+        'name': 'White Jersey',
+        'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+        'price': 59.99,
+        'pic': 'red_jersey.jpg',
+        'qty_in_stock':200
+    },
+    {
+        'id': 7,
+        'name': 'Yellow Jersey',
+        'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+        'price': 49.99,
+        'pic': 'white_jersey.jpg',
+        'qty_in_stock': 133
+    },
+    {
+        'id': 8,
+        'name': 'Blue Jersey',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'price': 54.99,
+        'pic': 'black_jersey.jpg',
+        'qty_in_stock': 544
+    },
+    {
+        'id': 9,
+        'name': 'Red Jacket',
+        'description': 'Blue Jacket for cold and raniy weather',
+        'price': 129.99,
+        'pic': 'blue_jacket.jpg',
+        'qty_in_stock': 14
+    },
+    {
+        'id': 10,
+        'name': 'Black Jersey',
+        'description': 'Manchester United New Era Snapback Cap- Adult',
+        'price': 124.99,
+        'pic': 'cap.jpg',
+        'qty_in_stock': 655
+    },
+    {
+        'id': 11,
+        'name': 'Champion Flag',
+        'description': 'Manchester United Champions League Flag',
+        'price': 24.99,
+        'pic': 'champion_league_flag.jpg',
+        'qty_in_stock': 321
+    }
+ ]
